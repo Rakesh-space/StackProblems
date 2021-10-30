1c1b2757cb595fa2935a60c8722095e782de2e00
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-30 14:07:05 +0530

    Adding UC1 created a stack

--- StackProblems/Program.cs
+++ StackProblems/Program.cs
@@ -6,7 +6,12 @@ namespace StackProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n Welcome Stack Problem..");
+            Console.WriteLine("\n Stack operation :");
+            UC1CreateStack st = new UC1CreateStack();
+            st.push(56);
+            st.push(30);
+            st.push(70);
+            st.display();
         }
     }
 }
---
+++ StackProblems/UC1CreateStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProblems
+{
+    class UC1CreateStack
+    {
+        private Node top;  //Node type of top is variable 
+
+        internal UC1CreateStack() //here constructor to initialize value to current instance top variable 
+        {
+            this.top = null;
+        }
+        internal void push(int data) //pushing the value into top of the stack
+        {
+            Node newNode = new Node(data);  //here newNode is created and to occuepied the memory and store the data
+            if (this.top == null)
+            {
+                newNode.next = this.top;  //newNode point to the top of the stack
+            }
+            this.top = newNode;  //newNode to assigned to the top of stack
+            Console.WriteLine(" {0} push to stack ", data);
+        }
+        internal void display()  //display the all stack of the data
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " "); //actual display the data in  stack of top
+                temp = temp.next;
+            }
+        }
+    }
+}
