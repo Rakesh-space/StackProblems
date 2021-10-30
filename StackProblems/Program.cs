93bb45e1e3b8432ca95b231e60d2ee35942eacdb
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-30 13:56:19 +0530

    Adding Welcome message

---
+++ StackProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome Stack Problem..");
+        }
+    }
+}
